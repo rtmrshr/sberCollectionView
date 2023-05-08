ad314875b0061bebf991b27771d3dd708b543589
Ratmir Sher <ratmir.sher@gmail.com>
2023-05-08 15:03:00 +0300

    Initial Commit

---
+++ sberCollectionView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  sberCollectionView
+//
+//  Created by  ratmir on 08.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
